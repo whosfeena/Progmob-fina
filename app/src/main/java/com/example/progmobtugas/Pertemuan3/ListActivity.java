e4ada30ca62bfcb85918f2d26d27ae8aa36bfdf0
whosfeena <finafebri233@gmail.com>
2020-10-06 00:20:34 +0700

    spinner

--- app/src/main/java/com/example/progmobtugas/Pertemuan3/ListActivity.java
+++ app/src/main/java/com/example/progmobtugas/Pertemuan3/ListActivity.java
@@ -3,8 +3,12 @@ package com.example.progmobtugas.Pertemuan3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.example.progmobtugas.R;
 
@@ -21,9 +25,31 @@ public class ListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_list);
 
         ListView lv = (ListView)findViewById(R.id.lvLatihan);
+        Spinner sp = (Spinner)findViewById(R.id.spinnerLatihan);
 
         //adapter digunakan untuk menghubungkan data dengan ui nya
         lv.setAdapter(new ArrayAdapter<String>(ListActivity.this,android.R.layout.simple_list_item_1,items));
+        ArrayAdapter<String> aa  = new ArrayAdapter<String>(ListActivity.this,android.R.layout.simple_spinner_item);
+        sp.setAdapter(aa);
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(ListActivity.this,"Anda Memilih " + items[i], Toast.LENGTH_LONG).show();
+            }
+        });
+
+        sp.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(ListActivity.this,"Anda Memilih " + items[i], Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                Toast.makeText(ListActivity.this,"Anda Tidak Memilih ", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
 }
